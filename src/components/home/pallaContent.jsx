81c4ab5d6ac6041e218009b0d47be38ee92af873
herohoro <mtwd.office.25@gmail.com>
2023-05-04 12:51:44 +0900

    Fix workSection_start

--- src/components/home/pallaContent.jsx
+++ src/components/home/pallaContent.jsx
@@ -32,56 +32,6 @@ function ContentBlock() {
           Aesthetic Cocumentarian
         </p>
       </div>
-      <div className={styles.buttonSection}>
-        <div className={styles.buttonFlex}>
-          <div
-            style={{
-              maxWidth: '100%',
-            }}
-          >
-            <span>Work</span>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.hr}></div>
-      <div
-        style={{
-          backgroundColor: 'rgba(31,33,49,0.3)',
-          fontSize: '0.75rem',
-          height: '1.375rem',
-          lineHeight: '1.375rem',
-          width: '100%',
-          position: 'relative',
-          display: 'flex',
-          padding: '0 0.6rem',
-          color: '#e6eb2f',
-        }}
-      >
-        <span
-          style={{
-            borderTop: 'dashed 2px',
-            height: 'inherit',
-            flexGrow: '1',
-            position: 'relative',
-            top: 'calc(50% - 1px)',
-          }}
-        >
-          &nbsp;
-        </span>
-        <span style={{ padding: '0 0.6rem' }}>#work</span>
-        <span
-          style={{
-            borderTop: 'dashed 2px',
-            height: 'inherit',
-            flexGrow: '1',
-            position: 'relative',
-            top: 'calc(50% - 1px)',
-          }}
-        >
-          &nbsp;
-        </span>
-      </div>
     </div>
   );
 }
